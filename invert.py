e7cb2929a82b983879514123cba1c2ad1c17a7a0
cgrima <cyril.grima@gmail.com>
2016-04-29 17:13:44 -0500

    optimized for normal incidence

--- invert.py
+++ invert.py
@@ -13,44 +13,60 @@ import matplotlib.pyplot as plt
 nan = float('nan')
 
 
-def srf2power(model, approx, h=nan, rx=nan, db=True, SAA=True, 
-                  kind='isotropic gaussian', **kwargs):
-    """Power components over circular footprint from surface properties
+def srf2power_norminc(model, approx, gain=lambda th:1, th_max=nan,
+                      db=True, kind='isotropic gaussian', **kwargs):
+    """Power components over circular footprint at normal incident
+    from surface properties
+
+    PARAMETERS
+    ==========
+    gain: function
+        function of the observation angle (radian) and return
+        a linear amplitude
+
+    th_max: float
+        Maximum observation angle corresponding to the edge of the footprint
+
     **kwargs: to be passed to the model (do not include theta)
 
-    EXAMPLE:
-    invert.power_SAA('iem','Small_S',h=1e3 r=100, wf=60e6,ep2=3,sh=1.,cl=100)
+    EXAMPLE
+    =======
+    In[1]: sr.invert.srf2power_norminc('iem','Small_S', th_max=0.008,
+           wf=13.78e9, ep2=1.5, sh=1.5e-3, cl=100e-3)
+    Out[1]:
+    {'pc': -23.172003557542929,
+    'pn': -36.370086693995717,
+    'ratio': 13.198083136452789}
     """
 
     m = import_module('subradar.' + model)
-    func = getattr(m, approx)
+    Scattering = getattr(m, approx)
 
     # Coherent Signal
-    a = func(th=0, **kwargs)
-    pc = a.R['nn']**2 * exp( -(2*a.wk*a.sh*cos(a.th))**2 )
+    a = Scattering(th=0, **kwargs)
+    pc = a.R['nn']**2 * exp( -(2*a.wk*a.sh)**2 )
 
-    # Incoherent signal
-    if SAA is True:
-        pn = integrate.quad(lambda x: func(th=x/h, **kwargs).nRCS(kind=kind
-                            )['hh'] \
-             *pi*x, 0, rx)[0]
-        pn = pn /(pi*h**2)
+    # Incoherent Signal
+    # Note: nRCS(th=0) approximation all over the footprint.
+    # That allows to get nRCS out of the integral for faster computation
+    nRCS = lambda th: Scattering(th=th, **kwargs).nRCS(kind=kind)['hh']
+    integrand = lambda th: 2* gain(th)**2 * np.arctan(th)/(th**2+1)
+    pn = integrate.quad(integrand, 0, th_max)[0] *nRCS(0)
 
+    # Output
     ratio = pc/pn
-
     if db:
         pc, pn, ratio = 10*log10(pc), 10*log10(pn), 10*log10(ratio)
-
     return {'pc':pc, 'pn':pn, 'ratio':ratio}
 
 
-def power2srf(model, approx, pc, pn, wf=nan, h=nan, rx=nan, db=True,
-                 kind='isotropic gaussian', ep_range=[1.4,2.5],
-                 cl_logrange=[-1, 2], n=50, SAA=True, verbose=False):
+def power2srf_norminc(model, approx, pc, pn, gain=lambda th:1, wf=nan,
+              th_max=nan, db=True, kind='isotropic gaussian',
+              ep_range=[1.4,2.5], cl_logrange=[-1, 2], n=50, verbose=False):
     """Surface properties from Power components [in dB]
     """
     pc = 10**(pc/10.)
-    s = Signal(wf=wf, bw=nan, th=rx/h)
+    s = Signal(wf=wf, bw=nan, th=th_max)
 
     ep = np.linspace(ep_range[0], ep_range[1], n)
     r = utils.R(1, ep, 1, 1, s.th)
@@ -67,8 +83,8 @@ def power2srf(model, approx, pc, pn, wf=nan, h=nan, rx=nan, db=True,
             print('\n')
         if sh[i] != 0: #if no solution for sh, do not compute
             for j in reversed(range(0, jn, 1)):
-                tmp = srf2power(model, approx, h=h, rx=rx, wf=wf, ep2=ep[i],
-                      sh=sh[i], cl=cl[j], SAA=SAA)['pn']
+                tmp = srf2power_norminc(model, approx, gain=gain, th_max=th_max, wf=wf,
+                      ep2=ep[i], sh=sh[i], cl=cl[j])['pn']
                 if verbose is True:
                     print('[%04d - %04d] ep = %05.2f, sh= %09.6f, cl = %08.3f, pn = %05.1f'
                           % (i, j, ep[i], sh[i], cl[j], tmp))
