cgrima/subradar

commit 6b03f801eb555743f9a42c57683e98960c806365

pylint cleanup

subradar/iem.py
@@ -43,13 +43,11 @@ class Common(Fresnel):
 
 class Large_S(Signal, Fresnel):
     """Large roughness single-scatter approximation [sec. 5.5]"""
-    pass
 
 
 
 class Large_M(Signal, Fresnel):
     """Large roughness multiple-scatter approximation [sec. 5.5]"""
-    pass
 
 
 
@@ -192,11 +190,11 @@ class Small_S(Common, Signal, Fresnel, Roughness):
 
             return out
 
-        if type(n) is not str:
+        if not isinstance(n, str):
             vv = nsum(lambda x: _nRCS(self, 'vv', x, kind, transmission), [1,n] )
             hh = nsum(lambda x: _nRCS(self, 'hh', x, kind, transmission), [1,n] )
 
-        if type(n) is str:
+        if not(isinstance(n, str):
             vv = nsum(lambda x: _nRCS(self, 'vv', x, kind, transmission), [1,inf], method=n)
             hh = nsum(lambda x: _nRCS(self, 'hh', x, kind, transmission), [1,inf], method=n)
 
@@ -211,4 +209,3 @@ class Small_S(Common, Signal, Fresnel, Roughness):
 
 class Small_M(Signal, Fresnel):
     """Small-to-moderate roughness multiple-scatter approximation [sec. 5.5]"""
-    pass

subradar/Classdef.py
@@ -4,12 +4,12 @@ __author__ = 'Cyril Grima'
 
 import numpy as np
 
-from . import roughness, surface, utils
+from . import utils
 
 NAN = float('nan')
 
 
-class Signal(object):
+class Signal:
     """Signal relationships"""
     def __init__(self, wf=NAN, bw=NAN, th=0., bmw=NAN, h=NAN, **kwargs):
         self.wf = wf  # Signal central frequency [Hz]
@@ -29,7 +29,7 @@ class Signal(object):
                              }
 
 
-class Fresnel(object):
+class Fresnel:
     """Fresnel and Snell's relationships"""
     def __init__(self, ep1=1., ep2=1., mu1=1., mu2=1., th=0, **kwargs):
         self.ep1 = ep1  # Electric permittivity 1
@@ -50,7 +50,7 @@ class Fresnel(object):
                   }
 
 
-class Roughness(object):
+class Roughness:
     """Roughness relationships"""
     def __init__(self, wf=NAN, sh=0, cl=np.inf, **kwargs):
         self.wf = wf  # Signal central frequency [Hz]

subradar/invert.py
@@ -1,12 +1,13 @@
 """Signal inversion"""
 
-from . import utils
-from .Classdef import Fresnel, Signal
+from importlib import import_module
 from scipy import integrate
 import numpy as np
 from numpy import cos, exp, log, log10, pi, sqrt
-from importlib import import_module
 import matplotlib.pyplot as plt
+
+from . import utils
+from .Classdef import Fresnel, Signal
 
 
 nan = float('nan')

subradar/surface.py
@@ -1,14 +1,15 @@
 """Various tools fpr surface detection"""
 
+import copy
+
 import numpy as np
 import pandas as pd
 import scipy.signal
 from . import utils
-import copy
 
 
 def detector(rdg, y0=None, winsize=100, method='grima2012', axis=0, **kwargs):
-    """Surface detection with the choosen method
+    """Surface detection with the chosen method
 
     Input
     -----
@@ -38,8 +39,7 @@ def detector(rdg, y0=None, winsize=100, method='grima2012', axis=0, **kwargs):
     if axis == 1:
         rdg = np.rot90(rdg)
 
-    xsize = rdg.shape[0]
-    ysize = rdg.shape[1]
+    xsize, ysize = rdg.shape
     y = np.zeros(xsize)
 
     # Detection
@@ -196,7 +196,7 @@ def gcc(rdg, tau_threshold=2, **kwargs):
 
 
     #----------------------------------------
-    # Vertical offset that correponsds to tau
+    # Vertical offset that correponds to tau
 
     offset = [np.sum(tau[:i]) for i in yn]
     offset = np.array(offset)

commit 65431759effa1a3f9b8e0666d0ec86ee18258e9d

Fix bug introduced in 6b03f80

subradar/iem.py
@@ -193,8 +193,7 @@ class Small_S(Common, Signal, Fresnel, Roughness):
         if not isinstance(n, str):
             vv = nsum(lambda x: _nRCS(self, 'vv', x, kind, transmission), [1,n] )
             hh = nsum(lambda x: _nRCS(self, 'hh', x, kind, transmission), [1,n] )
-
-        if not isinstance(n, str):
+        else:
             vv = nsum(lambda x: _nRCS(self, 'vv', x, kind, transmission), [1,inf], method=n)
             hh = nsum(lambda x: _nRCS(self, 'hh', x, kind, transmission), [1,inf], method=n)
 